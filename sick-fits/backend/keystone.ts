55b59bcdade51a7a249f0bdad2378905799641dd
Bruce Brophy <bruce@brucebrophy.com>
2021-02-15 10:24:02 -0500

    Adds schemas for products and images

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import { config, createSchema } from "@keystone-next/keystone/schema";
 import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
+import { ProductImage } from "./schemas/ProductImage";
 import { createAuth } from "@keystone-next/auth";
 import {
   withItemData,
   statelessSessions,
 } from "@keystone-next/keystone/session";
+import { insertSeedData } from "./seed-data";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sickfits";
@@ -36,10 +39,16 @@ export default withAuth(
     db: {
       adapter: "mongoose",
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        if (process.argv.includes("--seed-data")) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       User,
+      Product,
+      ProductImage,
     }),
     ui: {
       isAccessAllowed: ({ session }) => {
